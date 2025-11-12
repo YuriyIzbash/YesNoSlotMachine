323a77ac65a0a574395f8a93c83e33566dcb4764
Yuriy Izbash <izbash91@gmail.com>
2025-11-12 22:29:25 +0100

    Initial Commit

---
+++ WatchYesNoSlotMachine Watch App/WatchYesNoSlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchYesNoSlotMachineApp.swift
+//  WatchYesNoSlotMachine Watch App
+//
+//  Created by yuriy on 12. 11. 25.
+//
+
+import SwiftUI
+
+@main
+struct WatchYesNoSlotMachine_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YesNoSlotMachine/YesNoSlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  YesNoSlotMachineApp.swift
+//  YesNoSlotMachine
+//
+//  Created by yuriy on 12. 11. 25.
+//
+
+import SwiftUI
+
+@main
+struct YesNoSlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
